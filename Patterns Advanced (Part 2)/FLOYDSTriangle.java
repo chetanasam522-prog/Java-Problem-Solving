85d73340fa623075e99b42a2d03678dd3e79fd84
Chetana Sambare <chetanasam522@gmail.com>
2025-12-31 14:10:59 +0000

    Added new java programs

---
+++ Patterns Advanced (Part 2)/FLOYDSTriangle.java
@@ -0,0 +1,73 @@
+/** # Problem Statement :- Print FLOYD'S Triangle   1
+                                                    2 3
+                                                    4 5 6
+                                                    7 8 9 10  
+                                                    11 12 13 14 15
+    # Logic (Step by Step)
+    
+    1. Start the program
+     . Program execution begins from the main() method.
+    2. Call the Floyd’s Triangle method
+     . FLOYDSTriangle(n) method is called.
+     . n represents the number of rows to be printed.
+    3. Initialize a counter
+     . int counter = 1;
+     . This variable stores the number to be printed in the triangle.
+    4. Use an outer loop for rows
+     . for (int i = 1; i <= n; i++)
+     . This loop controls the number of rows.
+    5. Use an inner loop for numbers
+     . for (int j = 1; j <= i; j++)
+     . This loop prints numbers in each row.
+     . The number of elements printed in a row equals the row number.
+    6. Print the counter value
+     . System.out.print(counter + " ");
+     . Prints the current value of counter.
+    7. Increment the counter
+     . counter++;
+     . Ensures the next number is printed sequentially.
+    8. Move to the next line
+     . System.out.println();
+     . Moves the cursor to the next row after completing one row.
+    9. Repeat until all rows are printed
+     . Steps 5 to 8 repeat until n rows are completed.
+   10. End of the program
+     . After printing all rows, the program terminates.
+
+    # Time & Space Complexity :-
+
+    1. Time Complexity 
+     . Outer loop runs n times.
+     . Inner loop runs i times for each row
+     . Total Operations 1+2+3----n = n(n+1)/2
+     . Time Complexity = O(n Square)
+
+     2. Space Complexity
+      . No extra data structures are used.
+      . Only primitive variables (i, j, counter) are used.
+      . Space Complexity = O(n)
+
+     # Solution :-
+ */
+
+  public class FLOYDSTriangle{
+   public static void FLOYDSTriangle(int n){
+    int counter = 1;
+
+    // outer loop
+    for(int i=1; i<=n; i++){
+    
+    // Inner loop
+    for(int j=1; j<=i; j++){
+    
+    // Print Pattern
+    System.out.print(counter + " ");
+     counter++;
+    }
+    System.out.println();
+    }
+   }
+   public static void main(String args[]){
+    FLOYDSTriangle( 5);
+   }
+  }
---
+++ Patterns Advanced (Part 2)/InvertedRotetedHalfPyramid.java
@@ -0,0 +1,75 @@
+/** # Problem Statement :- Print Inverted & Roteted Half-Pyramid    *
+                                                                 *  *
+                                                              *  *  *
+                                                          *   *  *  *
+                                                        
+    # Logic (Step by Step) 
+    
+   1. Start the program
+    . Program execution begins from the main() method.
+   2. Call the pattern method
+    . The method InvertedRotetedHalfPyramid(n) is called.
+    . n represents the number of rows in the pyramid.
+   3. Use an outer loop for rows
+    . for (int i = 1; i <= n; i++)
+    . This loop controls the number of rows in the pattern.
+   4. Print spaces before stars
+    . for (int j = 1; j <= n - i; j++)
+    . This loop prints spaces before stars.
+    . As the row number increases, the number of spaces decreases.
+   5. Print stars
+    . for (int j = 1; j <= i; j++)
+    . This loop prints stars in each row.
+    . The number of stars printed is equal to the current row number.
+   6. Move to the next line
+    . System.out.println();
+    . After printing spaces and stars, the cursor moves to the next line.
+   7. Repeat until all rows are printed
+    . Steps 4 to 6 repeat until n rows are printed.
+
+    # Time & Space Complexity :-
+    1. Time Complexity 
+     . Let n be the number of rows.
+     . Loop Analysis
+     . Outer loop runs n times.
+     . First inner loop (spaces) runs (n − i) times for each row.
+     . Second inner loop (stars) runs i times for each row.
+     . Total operations per row
+     . (n-i)+i = n
+     . Time Complexity = O(n square)
+
+     2. Space Complexity
+      . No extra data structures (arrays, lists, etc.) are used.
+      . Only loop variables (i, j, n) are used.
+      . Memory usage does not depend on input size.
+      . Space Complexity = O(n)
+
+     # Solution :-
+ */
+
+public class InvertedRotetedHalfPyramid{
+    public static void InvertedRotetedHalfPyramid(int n){
+          
+        // Outer loop
+        for(int i=1; i<=n;i++){
+
+        // Inner loop (Space)
+        for(int j=1; j<=n-i;j++){
+        System.out.print(" ");
+        
+        }
+        // Inner loop (Star)
+        for(int j=1; j<=i; j++){
+        
+        // Print Pattern
+        System.out.print("*");
+    
+        }
+         System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        InvertedRotetedHalfPyramid(4);
+
+    }
+}
