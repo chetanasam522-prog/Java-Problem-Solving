33c677cd77028af51daa8cf52873c365a80591f0
Chetana Sambare <chetanasam522@gmail.com>
2026-01-06 04:12:05 +0000

    Added new java program

--- Function & Method/Basics.java
+++ Function & Method/Basics.java
@@ -51,5 +51,45 @@
     
  # Parameters:-
   1. Formal Parameter(Normal Parameter):-
-  
- */
+   . A formal parameter is a variable written in the method definition that receives values when the method is called.
+   . Syntax :-
+   public static void add(int a, int b) {
+    int sum = a + b;
+    System.out.println(sum);
+}
+    Here :-
+   . int a
+   . int b
+   These are formal parameters.
+
+  2. Actual Parameter :-
+   . Actual parameters are the real values given to a method during method calling.
+   . Syntax:-
+   public class Example {
+    static void add(int a, int b) {   // a, b → formal parameters
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+        add(5, 3);   // 5, 3 → actual parameters
+    }
+}
+Ex = Find Product A & B*/
+  import java.util.*;
+  public class Basics{
+  public static int  CalculateProd(int a,int b){
+   int prod = a*b;
+   return prod;
+  }
+   public static void main(String args[]){
+    Scanner Sc = new Scanner(System.in);
+    // Take input form user
+    int a = Sc.nextInt();
+    int b = Sc.nextInt();
+    int prod = CalculateProd(a,b);
+    System.out.println("a*b = "+prod);
+   }
+
+  }
+
+ 
